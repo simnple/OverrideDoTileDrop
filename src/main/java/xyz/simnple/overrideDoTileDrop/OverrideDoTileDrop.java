935efe1a25e38f39384f60c4aafe55cd8d956bd0
Simnple <75520229+simnple@users.noreply.github.com>
2025-01-21 00:46:50 +0900

    Initial commit

---
+++ src/main/java/xyz/simnple/overrideDoTileDrop/OverrideDoTileDrop.java
@@ -0,0 +1,48 @@
+package xyz.simnple.overrideDoTileDrop;
+
+import org.bukkit.GameRule;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OverrideDoTileDrop extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent e) {
+        if (e.isCancelled()) {
+            return;
+        }
+
+        if (e.getBlock().getType() != Material.SHULKER_BOX) {
+            return;
+        }
+
+        if (Boolean.TRUE.equals(e.getBlock().getWorld().getGameRuleValue(GameRule.DO_TILE_DROPS))) {
+            return;
+        }
+
+        if (!e.isDropItems()) {
+            return;
+        }
+
+        e.setDropItems(false);
+
+        for (ItemStack item : e.getBlock().getDrops()) {
+            e.getBlock().getWorld().dropItem(e.getBlock().getLocation().toCenterLocation(), item);
+        }
+    }
+}
